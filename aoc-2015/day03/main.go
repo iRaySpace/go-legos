79286264ca78ee9613183f3316d421e3b50a4ff5
irayspace <irayspacii@gmail.com>
2024-01-08 14:43:42 +0800

    feat: day 03 part 2 and minor adjustments

--- aoc-2015/day03/main.go
+++ aoc-2015/day03/main.go
@@ -12,7 +12,7 @@ func First() {
 		log.Fatal(err)
 	}
 	m := make(map[string]bool)
-	houses := 1 // Already visited in his location?!?
+	m["0,0"] = true
 	var x, y int
 	for _, ch := range data {
 		switch ch {
@@ -27,12 +27,53 @@ func First() {
 		}
 		m[fmt.Sprintf("%d,%d", x, y)] = true
 	}
-	houses = houses + len(m)
-	fmt.Println(houses)
+	fmt.Println(len(m))
 }
 
 func Second() {
-
+	data, err := os.ReadFile("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	m := make(map[string]bool)
+	m["0,0"] = true
+	var x, y int
+	var rx, ry int
+	for i, ch := range data {
+		robot := i%2 != 0
+		switch ch {
+		case '^':
+			if robot {
+				ry = ry + 1
+			} else {
+				y = y + 1
+			}
+		case 'v':
+			if robot {
+				ry = ry - 1
+			} else {
+				y = y - 1
+			}
+		case '>':
+			if robot {
+				rx = rx + 1
+			} else {
+				x = x + 1
+			}
+		case '<':
+			if robot {
+				rx = rx - 1
+			} else {
+				x = x - 1
+			}
+		}
+		if robot {
+			m[fmt.Sprintf("%d,%d", rx, ry)] = true
+		} else {
+			m[fmt.Sprintf("%d,%d", x, y)] = true
+		}
+	}
+	fmt.Println(len(m))
 }
 
 func main() {
