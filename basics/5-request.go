57b23818e104bfe92162d3fbf3224638d2c8f0bc
irayspace <irayspacii@gmail.com>
2024-01-09 11:58:55 +0800

    feat: request

---
+++ basics/5-request.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+	"os"
+)
+
+// DISCORD_URL=https://discord.com/api/webhooks/1234567890/abcdefghijklmnopqrstuvwxyz go run 5-request.go
+func main() {
+	discordUrl := os.Getenv("DISCORD_URL")
+	body := `{"content": "Hello, world!"}`
+	_, err := http.Post(discordUrl, "application/json", strings.NewReader(body))
+	if err != nil {
+		panic(err)
+	}
+}
