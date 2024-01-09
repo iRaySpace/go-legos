8a479fde9bc927a9056d0d33be6fbaf93f8b7fcc
irayspace <irayspacii@gmail.com>
2024-01-09 13:17:35 +0800

    feat: request discord with mention

--- basics/5-request.go
+++ basics/5-request.go
@@ -6,10 +6,11 @@ import (
 	"os"
 )
 
-// DISCORD_URL=https://discord.com/api/webhooks/1234567890/abcdefghijklmnopqrstuvwxyz go run 5-request.go
+// USER_ID=1234567890 DISCORD_URL=https://discord.com/api/webhooks/1234567890/abcdefghijklmnopqrstuvwxyz go run 5-request.go
 func main() {
 	discordUrl := os.Getenv("DISCORD_URL")
-	body := `{"content": "Hello, world!"}`
+	userId := os.Getenv("USER_ID")
+	body := `{"content": "Hello, <@` + userId + `>!"}`
 	_, err := http.Post(discordUrl, "application/json", strings.NewReader(body))
 	if err != nil {
 		panic(err)
